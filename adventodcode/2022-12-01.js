4f506158570a76fbc4d2a9bc92987ba8da70de86
Zharzhan Kulmyrza <hey@jarjan.xyz>
2022-12-01 10:36:07 +0100

    Advent of code 2022 day 1

---
+++ adventodcode/2022-12-01.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+var path = require("path");
+
+fs.readFile(path.join(__dirname, "./2022-12-01.txt"), "utf8", (err, str) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const strArr = str.split("\n");
+  const arr = [[]];
+  strArr.map((newStr) => {
+    if (newStr === "") {
+      arr.push([]);
+    } else {
+      arr[arr.length - 1].push(Number(newStr));
+    }
+  });
+  console.log("part1: ", part1(arr));
+  console.log("part2: ", part2(arr));
+});
+
+function sum(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0);
+}
+
+function part1(arr) {
+  return arr.reduce((acc, currArr) => Math.max(acc, sum(currArr)), 0);
+}
+
+console.log(
+  "test: ",
+  part1([[1000, 2000, 3000], [4000], [5000, 6000], [7000, 8000, 9000], [10000]])
+);
+
+const compareNumbers = (a, b) => a - b;
+
+function part2(arr) {
+  const sumArr = arr.map((currArr) => sum(currArr));
+
+  sumArr.sort(compareNumbers);
+  return sum(sumArr.slice(-3));
+}
