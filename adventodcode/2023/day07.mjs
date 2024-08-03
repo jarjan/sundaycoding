ad6cc89267dac54090795365887d9aea17cd9445
Zharzhan Kulmyrza <hey@jarjan.xyz>
2024-08-03 21:26:06 +0200

    aoc23 day 7 and 8 incomplete

---
+++ adventodcode/2023/day07.mjs
@@ -0,0 +1,51 @@
+import fs from "fs/promises";
+import path from "path";
+import url from "url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const inputFile = path.join(__dirname, "./input/day07.txt");
+
+fs.readFile(inputFile, { encoding: "utf8" })
+  .then((str) => {
+    const arr = str.split("\n");
+
+    // console.log("part1: ", part1(arr));
+    // console.log("part2: ", part2(arr));
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const stength = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "T",
+  "J",
+  "Q",
+  "K",
+  "A",
+];
+
+function part1(arr) {
+  return arr;
+}
+
+console.log(
+  "test 1:",
+  part1(["32T3K 765", "T55J5 684", "KK677 28", "KTJJT 220", "QQQJA 483"])
+);
+
+function part2(arr) {
+  return arr;
+}
+
+// console.log(
+//   "test 2:",
+//   part2(["32T3K 765", "T55J5 684", "KK677 28", "KTJJT 220", "QQQJA 483"])
+// );
