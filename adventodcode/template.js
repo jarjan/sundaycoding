62b35aacc537d4e2fb66d5fa9ca37f973e21cbc9
Zharzhan Kulmyrza <zharzhan.kulmyrza@deliveryhero.com>
2025-12-02 18:32:01 +0100

    es modules by default

---
+++ adventodcode/template.js
@@ -0,0 +1,35 @@
+import fs from "fs/promises";
+import path from "path";
+import url from "url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const inputFile = path.join(__dirname, "./input/day01.txt");
+
+fs.readFile(inputFile, { encoding: "utf8" })
+  .then((str) => {
+    const arr = str.split("\n");
+
+    console.log("part1: ", part1(arr));
+    console.log("part2: ", part2(arr));
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const test = [];
+
+function part1(arr) {
+  let sum = 0;
+
+  return sum;
+}
+
+console.log("test1: ", part1(test));
+
+function part2(arr) {
+  let sum = 0;
+
+  return sum;
+}
+
+console.log("test2: ", part2(test));
