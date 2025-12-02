ee58fd9898ed96891404b4153e590c846cedf741
Zharzhan Kulmyrza <zharzhan.kulmyrza@deliveryhero.com>
2025-12-02 20:13:00 +0100

    aoc25 day02 part2

--- adventodcode/2025/day02.js
+++ adventodcode/2025/day02.js
@@ -10,11 +10,25 @@ fs.readFile(inputFile, { encoding: "utf8" })
     const arr = str.split(",");
 
     console.log("part1: ", part1(arr));
-    // console.log("part2: ", part2(arr));
+    console.log("part2: ", part2(arr));
   })
   .catch((err) => {
     console.error(err);
   });
+
+const test = [
+  "11-22",
+  "95-115",
+  "998-1012",
+  "1188511880-1188511890",
+  "222220-222224",
+  "1698522-1698528",
+  "446443-446449",
+  "38593856-38593862",
+  "565653-565659",
+  "824824821-824824827",
+  "2121212118-2121212124",
+];
 
 function part1(arr) {
   let sum = BigInt(0);
@@ -37,42 +51,36 @@ function part1(arr) {
   return sum.toString();
 }
 
-console.log(
-  "test1: ",
-  part1([
-    "11-22",
-    "95-115",
-    "998-1012",
-    "1188511880-1188511890",
-    "222220-222224",
-    "1698522-1698528",
-    "446443-446449",
-    "38593856-38593862",
-    "565653-565659",
-    "824824821-824824827",
-    "2121212118-2121212124",
-  ])
-);
+console.log("test1: ", part1(test));
 
 function part2(arr) {
-  let sum = 0;
+  let sum = BigInt(0);
 
-  return sum;
+  arr.forEach((str) => {
+    const [one, two] = str.split("-");
+    const first = BigInt(one);
+    const second = BigInt(two);
+    for (let i = first; i <= second; i++) {
+      const num = i.toString();
+      const len = num.length;
+      const half = Math.floor(len / 2);
+
+      for (let j = half; j >= 1; j--) {
+        if (len % j === 0) {
+          const chunks = [];
+          for (let k = 0; k < len; k += j) {
+            chunks.push(num.slice(k, k + j));
+          }
+          if (chunks.every((chunk) => chunk === chunks[0])) {
+            sum += i;
+            break;
+          }
+        }
+      }
+    }
+  });
+
+  return sum.toString();
 }
 
-// console.log(
-//   "test2: ",
-//   part2([
-//     "11-22",
-//     "95-115",
-//     "998-1012",
-//     "1188511880-1188511890",
-//     "222220-222224",
-//     "1698522-1698528",
-//     "446443-446449",
-//     "38593856-38593862",
-//     "565653-565659",
-//     "824824821-824824827",
-//     "2121212118-2121212124",
-//   ])
-// );
+console.log("test2: ", part2(test));
