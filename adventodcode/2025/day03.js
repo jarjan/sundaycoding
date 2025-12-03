dea68dcd165c92a48c1badd145dd4749613dfa34
Zharzhan Kulmyrza <zharzhan.kulmyrza@deliveryhero.com>
2025-12-03 19:29:31 +0100

    aoc25 day03 part1

---
+++ adventodcode/2025/day03.js
@@ -0,0 +1,49 @@
+import fs from "fs/promises";
+import path from "path";
+import url from "url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const inputFile = path.join(__dirname, "./input/day03.txt");
+
+fs.readFile(inputFile, { encoding: "utf8" })
+  .then((str) => {
+    const arr = str.split("\n");
+
+    console.log("part1: ", part1(arr));
+    // console.log("part2: ", part2(arr));
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const test = [
+  "987654321111111",
+  "811111111111119",
+  "234234234234278",
+  "818181911112111",
+];
+
+function part1(arr) {
+  let sum = 0;
+  arr.forEach((str) => {
+    let max = 0;
+    for (let i = 0; i < str.length - 1; i++) {
+      for (let j = i + 1; j < str.length; j++) {
+        max = Math.max(max, parseInt(str[i] + str[j]));
+      }
+    }
+    sum += max;
+  });
+
+  return sum;
+}
+
+console.log("test1: ", part1(test));
+
+function part2(arr) {
+  let sum = 0;
+
+  return sum;
+}
+
+// console.log("test2: ", part2(test));
