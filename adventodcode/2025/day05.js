a0d9dbb7aa3bbc8dc5bd8d19029e5cf178d63bdd
Zharzhan Kulmyrza <zharzhan.kulmyrza@deliveryhero.com>
2025-12-05 18:43:15 +0100

    aoc25 day05 (part1 + incomplete part2)

---
+++ adventodcode/2025/day05.js
@@ -0,0 +1,96 @@
+import fs from "fs/promises";
+import path from "path";
+import url from "url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const inputFile = path.join(__dirname, "./input/day05.txt");
+
+fs.readFile(inputFile, { encoding: "utf8" })
+  .then((str) => {
+    const arr = str.split("\n").reduce(
+      (acc, curr) => {
+        if (curr.includes("-")) {
+          acc[0].push(curr);
+        } else if (curr !== "") {
+          {
+            acc[1].push(curr);
+          }
+        }
+        return acc;
+      },
+      [[], []]
+    );
+
+    console.log("part1: ", part1(arr));
+    console.log("part2: ", part2(arr));
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const test = [
+  ["3-5", "10-14", "16-20", "12-18"],
+  ["1", "5", "8", "11", "17", "32"],
+];
+
+function part1(arr) {
+  let sum = 0;
+  const ranges = arr[0].map((str) => {
+    return str.split("-").map((num) => parseInt(num));
+  });
+  const nums = arr[1].map((str) => parseInt(str));
+
+  nums.forEach((num) => {
+    let found = ranges.some((range) => {
+      return num >= range[0] && num <= range[1];
+    });
+
+    if (found) {
+      sum++;
+    }
+  });
+
+  return sum;
+}
+
+console.log("test1: ", part1(test));
+
+function part2(arr) {
+  let sum = 0;
+  const lower = 0;
+  const upper = 1;
+
+  const ranges = arr[0].map((str) => {
+    const range = str.split("-").map((num) => parseInt(num));
+
+    return [range[lower], range[upper]];
+  });
+  ranges.sort((a, b) => a[0] - b[0]);
+
+  const merged = [];
+
+  let skip = false;
+  for (let i = 0; i < ranges.length; i++) {
+    const curr = ranges[i];
+    const next = ranges[i + 1];
+
+    if (curr[upper] >= next?.[lower]) {
+      merged.push([curr[lower], Math.max(curr[upper], next[upper])]);
+      skip = true;
+    } else {
+      if (!skip) {
+        merged.push(curr);
+      }
+
+      skip = false;
+    }
+  }
+
+  for (let i = 0; i < merged.length; i++) {
+    sum += merged[i][1] - merged[i][0] + 1;
+  }
+
+  return sum;
+}
+
+console.log("test2: ", part2(test));
