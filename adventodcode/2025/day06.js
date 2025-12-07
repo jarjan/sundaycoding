acbce87f7b4f0cd125f440d4bec7cfd43920f43c
Zharzhan Kulmyrza <zharzhan.kulmyrza@deliveryhero.com>
2025-12-07 20:46:57 +0100

    aoc25 day06 part1

---
+++ adventodcode/2025/day06.js
@@ -0,0 +1,62 @@
+import fs from "fs/promises";
+import path from "path";
+import url from "url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const inputFile = path.join(__dirname, "./input/day06.txt");
+
+fs.readFile(inputFile, { encoding: "utf8" })
+  .then((str) => {
+    const arr = str.split("\n");
+
+    console.log("part1: ", part1(arr));
+    // console.log("part2: ", part2(arr));
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const test = [
+  "123 328  51 64",
+  "45 64  387 23",
+  "6 98  215 314",
+  "*   +   *   + ",
+];
+
+function part1(arr) {
+  let sum = 0;
+  const ops = arr[arr.length - 1].split(" ").filter((item) => item !== "");
+
+  const nums = arr.slice(0, arr.length - 1).map((str) =>
+    str
+      .split(" ")
+      .filter((item) => item !== "")
+      .map((item) => parseInt(item))
+  );
+
+  for (let i = 0; i < nums[0].length; i++) {
+    sum += nums.reduce((acc, curr) => {
+      if (ops[i] === "+") {
+        acc += curr[i];
+      } else {
+        acc *= curr[i];
+      }
+      return acc;
+    }, 1);
+    if (ops[i] === "+") {
+      sum--;
+    }
+  }
+
+  return sum;
+}
+
+console.log("test1: ", part1(test));
+
+function part2(arr) {
+  let sum = 0;
+
+  return sum;
+}
+
+console.log("test2: ", part2(test));
